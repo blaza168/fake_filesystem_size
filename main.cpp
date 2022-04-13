44f6d317da8bc3f0db27603510942f7e78a57307
blaza168 <jan.blazek10@gmail.com>
2022-04-13 14:36:11 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+unsigned short counter = 0;
+
+unsigned long get_file_size(string path) {
+    unsigned long size;
+    try {
+        ifstream in (path, ifstream::ate | ifstream::binary);
+        size = in.tellg();
+        in.close();
+    } catch (std::exception const& e){
+        return 0;
+    }
+
+    return size;
+}
+
+void print(string path, unsigned long long size) {
+    string size_str = std::to_string(size);
+    string spaces = "";
+    int space_base = size_str.length() < 8 ? 10 : 14;
+    int size_len = size_str.length();
+
+    for (int i = 0; i < space_base - size_len; i++) {
+        spaces.append(" ");
+    }
+
+    std::cout << size_str << spaces << path << endl;
+}
+
+unsigned long long iterate_directory(string dir) {
+    unsigned long long size = 0;
+    try {
+        for (auto& p: fs::directory_iterator(dir)) {
+            if (fs::is_directory(p.path())) {
+                unsigned long tmp_size = iterate_directory(p.path());
+                print(p.path(), tmp_size);
+            } else if (fs::is_regular_file(p.path())) {
+                counter++;
+                if (counter == 14) {
+                    counter = 0;
+                    unsigned long tmp_size = get_file_size(p.path());
+                    if (tmp_size >= 1000000000 && tmp_size > 0) {
+                        continue; // Skip garbage files
+                    }
+                    print(p.path(), tmp_size);
+                    size += tmp_size;
+                }
+            }
+        }
+    } catch (std::exception const& e) {
+        return 0;
+    }
+
+    return size;
+}
+
+int main(int argc, char *argv[]) {
+    unsigned long long total_size = 0;
+    vector<string> scan_folders = {"/boot", "/cdrom", "/etc", "/home", "/lib", "/lib64", "/media", "/mnt", "/opt",
+                                   "/var", "/usr", "/run", "/root", "/sbin", "/tmp"};
+//    vector<string> scan_folders = {"/home/jan/Desktop/directory/school/4/cplusplus/du_override"};
+    vector<string>::iterator it;
+
+    for (it = scan_folders.begin(); it != scan_folders.end(); it++) {
+        unsigned long dir_size = iterate_directory(*it);
+        total_size += dir_size;
+        print(*it, dir_size);
+    }
+
+    print("/", total_size);
+    print("total", total_size);
+    return 0;
+}
